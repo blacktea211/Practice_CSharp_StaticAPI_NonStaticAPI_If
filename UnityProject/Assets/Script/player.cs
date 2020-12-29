cba39ac5eed2921cd0c9ca07e3fc73a61c8130eb
blacktea211 <70305824+blacktea211@users.noreply.github.com>
2020-12-29 14:37:51 +0800

    沒音效被打沒扣血提示窗沒顯示資訊

---
+++ UnityProject/Assets/Script/player.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [Header("血量")]
+    public float hp = 100f;
+    [Header("攻擊")]
+    public float atk;
+    [Header("音源")]
+    public AudioSource source;
+    [Header("音效")]
+    public AudioClip sound;
+    [Header("喪屍")]
+    public zombie zombie;
+
+    public float damage;
+
+    public void Attack()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            atk = Random.Range(0f, 50f);
+        }
+      }
+
+
+
+    public void Damage()
+    {
+        atk =- damage;
+        hp = 100 - atk;
+         print("<Color.red >+喪屍受到的傷害" + zombie.damage);
+
+        
+        if (zombie.hp <= 0)
+        {
+            Destroy(GetComponent<zombie>());
+            print("<Color.red>+喪屍已死亡");
+        }
+
+
+
+
+    }
+
+
+    void Start()
+    {
+            
+    }
+
+    
+    void Update()
+    {
+        Attack();
+    }
+}
---
+++ UnityProject/Assets/Script/zombie.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class zombie : MonoBehaviour
+{
+    [Header("血量")]
+    public float hp = 100f;
+    [Header("攻擊")]
+    public float atk;
+    [Header("音源")]
+    public AudioSource source;
+    [Header("音效")]
+    public AudioClip sound;
+    [Header("玩家")]
+    public player player;
+
+    public float damage;
+
+
+    public void Attack()
+    {
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            atk = Random.Range(0f, 50f);
+        }
+
+    }
+
+
+    public void Damage()
+    {
+        atk =- damage;
+
+        print("<Color.red >+角色受到的傷害" + player.damage);
+
+        if (player.hp <= 0)
+        {
+            Destroy(GetComponent<player>());
+            print("<Color.red>+角色已死亡");
+        }
+
+
+
+
+    }
+
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+        Attack();
+    }
+} 
